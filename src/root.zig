4d7de6f64280a809258e449db0680cea3e200001
Priyanshu Das <ask.priyanshu@gmail.com>
2024-07-28 15:21:23 +0530

    Added Better Error Handling and Implemented a custom character structure

--- src/root.zig
+++ src/root.zig
@@ -4,6 +4,34 @@ const raylib = @import("raylib");
 var prng = std.Random.DefaultPrng.init(42);
 const rand = prng.random();
 
+const TrailCharacter: type = struct {
+    character: i32,
+    changeFrame: c_int,
+
+    pub fn init(character: i32, changeFrame: c_int) TrailCharacter {
+        return TrailCharacter{ .character = character, .changeFrame = changeFrame };
+    }
+
+    pub fn getCharacter(self: *TrailCharacter) i32 {
+        return self.character;
+    }
+
+    pub fn setCharacter(self: *TrailCharacter, character: i32) void {
+        self.character = character;
+    }
+    pub fn getChangeFrame(self: *TrailCharacter) c_int {
+        return self.changeFrame;
+    }
+
+    pub fn setChangeFrame(self: *TrailCharacter, changeFrame: c_int) void {
+        self.changeFrame = changeFrame;
+    }
+
+    fn get_random_char() i32 {
+        return @as(i32, @intCast(rand_range(1, 255)));
+    }
+};
+
 const Trail: type = struct {
     x: c_int,
     y: c_int,
@@ -12,13 +40,13 @@ const Trail: type = struct {
     trailLength: usize,
     textColor: raylib.Color,
     depthView: c_int,
-    trail_array: []i32,
+    trail_array: []TrailCharacter,
 
-    pub fn init(x: c_int, y: c_int, fallSpeed: c_int, textSize: c_int, trailLength: usize, depthView: c_int, textColor: raylib.Color, array: []i32) !Trail {
+    pub fn init(x: c_int, y: c_int, fallSpeed: c_int, textSize: c_int, trailLength: usize, depthView: c_int, textColor: raylib.Color, array: []TrailCharacter) !Trail {
 
         //Unsafe Not Freeing the allocator
         const allocator = std.heap.page_allocator;
-        const trail_array: []i32 = try allocator.alloc(i32, trailLength);
+        const trail_array: []TrailCharacter = try allocator.alloc(TrailCharacter, trailLength);
         // defer allocator.free(trail_array);
 
         for (0..trailLength) |i| {
@@ -92,11 +120,11 @@ const Trail: type = struct {
         return self.textColor;
     }
 
-    pub fn getTrailArray(self: *Trail) []i32 {
+    pub fn getTrailArray(self: *Trail) []TrailCharacter {
         return self.trail_array;
     }
 
-    pub fn setTrailArray(self: *Trail, trail_array: []i32) void {
+    pub fn setTrailArray(self: *Trail, trail_array: []TrailCharacter) void {
         for (0..self.trailLength) |i| {
             self.trail_array[i] = trail_array[i];
         }
@@ -110,13 +138,13 @@ const windowFPS: c_int = 30;
 var frameCount: c_int = 0;
 const font_path = "assets/dejavu.fnt";
 const icon_path = "assets/icon.png";
+const globalTextSize: c_int = 30;
 
 pub fn main() !void {
 
     // Initialization
     const backgroundColor = raylib.Color.init(40, 44, 52, 100);
     const textColor = raylib.Color.init(65, 253, 254, 255);
-    const textSize: c_int = 30;
 
     raylib.initWindow(screenWidth, screenHeight, windowTitle);
     defer raylib.closeWindow();
@@ -140,7 +168,9 @@ pub fn main() !void {
     const trails: []Trail = try allocator.alloc(Trail, numberOfTrails);
     defer allocator.free(trails);
 
-    try create_trails(trails, numberOfTrails, textSize, textColor);
+    create_trails(trails, numberOfTrails, textColor) catch |err| {
+        std.debug.print("Error {}", .{err});
+    };
 
     // Main game loop
     while (!raylib.windowShouldClose()) {
@@ -152,8 +182,8 @@ pub fn main() !void {
         raylib.beginDrawing();
         defer raylib.endDrawing();
 
-        try update_trails(trails, numberOfTrails);
-        try draw_trails(trails, numberOfTrails, font);
+        update_trails(trails, numberOfTrails);
+        draw_trails(trails, numberOfTrails, font);
 
         frameCount = @mod(frameCount, 1000) + 1;
     }
@@ -163,87 +193,67 @@ pub fn main() !void {
     }
 }
 
-fn create_trails(trails: []Trail, numberOfTrails: usize, textSize: c_int, textColor: anytype) !void {
+fn create_trails(trails: []Trail, numberOfTrails: usize, textColor: anytype) !void {
     for (0..numberOfTrails) |i| {
         const x_val = rand_range(0, screenWidth);
         const y_val = rand_range(-100, screenHeight);
         const depth = rand_range(0, 4);
         // std.debug.print("{}\n", .{x_val});
-        const length = @as(usize, @intCast(rand_range(10, 20)));
 
+        const length = @as(usize, @intCast(rand_range(10, 20)));
         const allocator = std.heap.page_allocator;
-        var characters: []i32 = try allocator.alloc(i32, length);
+        var characters: []TrailCharacter = try allocator.alloc(TrailCharacter, length);
         defer allocator.free(characters);
 
         for (0..length) |j| {
-            characters[j] = get_random_char();
+            characters[j] = TrailCharacter.init(TrailCharacter.get_random_char(), rand_range(1, 30));
         }
-        const trail = try Trail.init(x_val, y_val, rand_range(10, 20), generateDepthBasedTextSize(textSize, depth), length, depth, textColor, characters);
+        const trail = try Trail.init(x_val, y_val, rand_range(10, 20), generateDepthBasedTextSize(globalTextSize, depth), length, depth, textColor, characters);
         trails[i] = trail;
     }
 }
 
-fn update_trails(trails: []Trail, numberOfTrails: usize) !void {
+fn update_trails(trails: []Trail, numberOfTrails: usize) void {
     for (0..numberOfTrails) |i| {
         var newY: c_int = trails[i].getY() + trails[i].getFallSpeed();
         if ((newY - trails[i].getTextSize() * @as(c_int, @intCast(trails[i].getTrailLength())) > screenHeight) or (trails[i].getX() < 0 or trails[i].getX() > screenWidth)) {
             newY = rand_range(-100, 0);
-            // const depth = rand_range(0, 4);
-            // trails[i].setTextSize(generateDepthBasedTextSize(30, depth));
+            const depth = rand_range(0, 4);
+            trails[i].setTextSize(generateDepthBasedTextSize(globalTextSize, depth));
             trails[i].setX(rand_range(0, screenWidth));
         }
         trails[i].setY(newY);
 
-        if (@mod(newY, 10) == 0) {
-            const length = trails[i].getTrailLength();
-            const allocator = std.heap.page_allocator;
-            var characters: []i32 = try allocator.alloc(i32, length);
-            defer allocator.free(characters);
-
-            for (0..length) |j| {
-                characters[j] = get_random_char();
+        for (0..trails[i].getTrailLength()) |j| {
+            if (@mod(frameCount, trails[i].getTrailArray()[j].getChangeFrame()) == 0) {
+                trails[i].getTrailArray()[j].setCharacter(TrailCharacter.get_random_char());
             }
-            trails[i].setTrailArray(characters);
         }
-
-        // if (@mod(frameCount, 2) == 0) {
-        //     if (trails[i].getX() < @divFloor(screenWidth, 2)) {
-        //         trails[i].setX(trails[i].getX() - 1);
-        //     } else {
-        //         trails[i].setX(trails[i].getX() + 1);
-        //     }
-        // }
-        // if (@mod(frameCount, 10) == 0) {
-        //     trails[i].setTextSize(trails[i].getTextSize() + 1);
-        // }
     }
 }
 
-fn draw_trails(trails: []Trail, numberOfTrails: usize, font: raylib.Font) !void {
+fn draw_trails(trails: []Trail, numberOfTrails: usize, font: raylib.Font) void {
     for (0..numberOfTrails) |i| {
         for (0..trails[i].getTrailLength()) |j| {
             const opacity = 1.5 * @as(f32, @floatFromInt(trails[i].getTrailLength() - j)) / @as(f32, @floatFromInt(trails[i].getTrailLength()));
             // const opacity = 1;
             const pos = raylib.Vector2.init(@as(f32, @floatFromInt(trails[i].getX())), @as(f32, @floatFromInt(trails[i].getY() - @as(c_int, @intCast(j)) * trails[i].getTextSize())));
+            const character = trails[i].trail_array[j].getCharacter();
             if (j == 0) {
-                raylib.drawTextCodepoint(font, trails[i].trail_array[j], pos, @as(f32, @floatFromInt(trails[i].getTextSize())), raylib.Color.init(1, 253, 251, 255));
+                raylib.drawTextCodepoint(font, character, pos, @as(f32, @floatFromInt(trails[i].getTextSize())), raylib.Color.init(1, 253, 251, 255));
             } else {
-                raylib.drawTextCodepoint(font, trails[i].trail_array[j], pos, @as(f32, @floatFromInt(trails[i].getTextSize())), trails[i].getTextColor().fade(opacity));
+                raylib.drawTextCodepoint(font, character, pos, @as(f32, @floatFromInt(trails[i].getTextSize())), trails[i].getTextColor().fade(opacity));
             }
         }
     }
 }
 
 fn generateDepthBasedTextSize(textSize: c_int, depth: c_int) c_int {
-    const size: c_int = @divFloor(textSize, depth + 1);
-    return size;
+    const depthBasedTextSize: c_int = @divFloor(textSize, depth + 1);
+    return depthBasedTextSize;
 }
 
 fn rand_range(x: c_int, y: c_int) c_int {
     const num: c_int = rand.intRangeAtMost(c_int, x, y);
     return num;
-}
-
-fn get_random_char() i32 {
-    return @as(i32, @intCast(rand_range(1, 255)));
 }
